973645484b3449af9a0916cbf602f58773b8c5b4
Jacob Liddy <liddyjacob@gmail.com>
2017-10-07 14:56:54 -0400

    Client and server working as desired

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
 #The job of thd
-def client_send(port, host, info):
+def client_send(host, port, info):
 	client = socket.socket()
 	client.connect((host, port))
 	client.send(info)
--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 #run.py
 #Run the whole thing
 
-import client
+from client import *
 import dog_device
-import 
+
+HOST = '18.220.180.253' 
+PORT = 50000
+
+client = socket.socket()
+client.connect((HOST, PORT))
+
+while True:
+	button = raw_input(">")
+	if button == "quit":
+		break
+	client_send(HOST, PORT, "DEVICE")	
+#	client_send(HOST, PORT, button)
+
+	
+client.close()
+
