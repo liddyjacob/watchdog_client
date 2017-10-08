7fbdfd2a175bce6adbd64fed0710074a021b8185
liddyjacob <liddyjacob@gmail.com>
2017-10-08 11:20:04 +0000

    version one of the server

--- run.py
+++ run.py
@@ -1,21 +1,32 @@
 #run.py
 #Run the whole thing
 
+PIN = 29
+HOST = '18.220.180.253'
+PORT = 64646
+
+
 from client import *
 import dog_device
+import time
+import mraa
 
-HOST = '18.220.180.253' 
-PORT = 50000
+button= mraa.Gpio(PIN)
+button.dir(mraa.DIR_OUT)
 
-client = socket.socket()
-client.connect((HOST, PORT))
 
 while True:
-	button = raw_input(">")
-	if button == "quit":
-		break
-	client_send(HOST, PORT, "DEVICE")	
-#	client_send(HOST, PORT, button)
+    
+    state = button.read()
+    if state == 1:
+        client_send(HOST, PORT, "DEVICE")	
+        time.sleep(5)
+        state = button.read()
+
+        if state == 1: 
+            break;
+        
+        
 
 	
 client.close()
